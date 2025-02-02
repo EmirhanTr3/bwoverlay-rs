827214d8615b6b49d0aab69df4bde9f33e45cb6e
EmirhanTr3 <emirhaneren20072010@gmail.com>
2025-02-03 00:12:05 +0300

    pain

---
+++ src/hypixel.rs
@@ -0,0 +1,110 @@
+#[allow(dead_code)]
+
+use serde_derive::Deserialize;
+
+use crate::Uuid;
+
+pub const BASE: f32 = 10000.0;
+pub const GROWTH: f32 = 2500.0;
+
+const HALF_GROWTH: f32 = GROWTH * 0.5;
+
+const REVERSE_PQ_PREFIX: f32 = -(BASE - 0.5 * GROWTH) / GROWTH;
+const REVERSE_CONST: f32 = REVERSE_PQ_PREFIX * REVERSE_PQ_PREFIX;
+const GROWTH_DIVIDES_2: f32 = 2.0 / GROWTH;
+
+#[derive(Deserialize, Debug)]
+pub struct HypixelPlayer {
+    pub name: String,
+    pub uuid: Uuid,
+    pub rank: String,
+    pub network_xp: i32,
+    pub network_level: i32,
+    pub level: i32,
+    pub winstreak: i32,
+    pub fkdr: f32,
+    pub wlr: f32,
+    pub final_kills: i32,
+    pub wins: i32,
+    pub bed_break: i32
+}
+
+impl HypixelPlayer {
+    pub fn from_api(raw_info: ApiHypixelPlayer, player_uuid: Uuid) -> Self {
+        HypixelPlayer {
+            name: raw_info.clone().name,
+            uuid: player_uuid,
+            rank: if raw_info.clone().monthly_package_rank.unwrap_or("".to_string()) == "SUPERSTAR" {
+                "MVP++".to_string()
+            } else {
+                raw_info.clone().new_package_rank.unwrap_or("Default".to_string()).replace("_PLUS", "+")
+            },
+            network_xp: raw_info.clone().network_xp.unwrap_or(0),
+            network_level: calculate_level(raw_info.clone().network_xp.unwrap_or(-1) as f32).round() as i32,
+            level: raw_info.clone().achievements.and_then(|a| a.bedwars_level).unwrap_or(-1),
+            winstreak: raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.winstreak).unwrap_or(-1),
+            fkdr: {
+                let final_kills = raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.final_kills_bedwars).unwrap_or(-1);
+                let final_deaths = raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.final_deaths_bedwars).unwrap_or(-1);
+                final_kills as f32 / final_deaths as f32
+            },
+            wlr: {
+                let wins = raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.wins_bedwars).unwrap_or(-1);
+                let losses = raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.losses_bedwars).unwrap_or(-1);
+                wins as f32 / losses as f32
+            },
+            final_kills: raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.final_kills_bedwars).unwrap_or(-1),
+            wins: raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.wins_bedwars).unwrap_or(-1),
+            bed_break: raw_info.clone().stats.and_then(|a| a.bedwars).and_then(|b| b.beds_broken_bedwars).unwrap_or(-1),
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct ApiHypixelData {
+    pub player: Option<ApiHypixelPlayer>
+}
+
+#[derive(Deserialize, Clone)]
+pub struct ApiHypixelPlayer {
+    #[serde(rename = "displayname")]
+    name: String,
+    #[serde(rename = "monthlyPackageRank")]
+    monthly_package_rank: Option<String>,
+    #[serde(rename = "newPackageRank")]
+    new_package_rank: Option<String>,
+    #[serde(rename = "networkExp")]
+    network_xp: Option<i32>,
+    achievements: Option<ApiAchievements>,
+    stats: Option<ApiStats>
+}
+
+#[derive(Deserialize, Clone)]
+struct ApiAchievements {
+    bedwars_level: Option<i32>
+}
+
+#[derive(Deserialize, Clone)]
+struct ApiStats {
+    #[serde(rename = "Bedwars")]
+    bedwars: Option<ApiBedwarsStats>
+}
+
+#[derive(Deserialize, Clone)]
+struct ApiBedwarsStats {
+    winstreak: Option<i32>,
+    final_kills_bedwars: Option<i32>,
+    final_deaths_bedwars: Option<i32>,
+    wins_bedwars: Option<i32>,
+    losses_bedwars: Option<i32>,
+    beds_broken_bedwars: Option<i32>,
+}
+
+
+fn calculate_level(exp: f32) -> f32 {
+    if exp < 0.0 {
+        1.0
+    } else {
+        (1.0 + REVERSE_PQ_PREFIX + (REVERSE_CONST + GROWTH_DIVIDES_2 * exp).sqrt()).floor()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,188 @@
+use std::{sync::Arc, collections::HashMap};
+use hotwatch::{Hotwatch, EventKind};
+use hypixel::{ApiHypixelData, HypixelPlayer};
+use regex::Regex;
+use tokio::{runtime::Runtime, fs::{self, File}, io::AsyncWriteExt};
+use anyhow::Result;
+use serde_derive::{Deserialize, Serialize};
+use log::{trace, warn, error};
+use reqwest::Client;
+use serde_json::json;
+
+type Uuid = String;
+
+mod hypixel;
+
+#[derive(Deserialize, Serialize, Clone)]
+struct Config {
+    #[serde(rename = "log-path")]
+    log_path: String,
+    #[serde(rename = "api-key")]
+    api_key: String,
+    #[serde(rename = "quit-level")]
+    quit_level: i32,
+}
+
+impl std::default::Default for Config {
+    fn default() -> Self {
+        let mut log_path = dirs::home_dir().unwrap();
+        #[cfg(target_os = "windows")]
+        {
+            log_path.push("AppData");
+            log_path.push("Roaming");
+        }
+        log_path.push(".minecraft");
+        log_path.push("logs");
+
+        Config {
+            log_path: log_path.display().to_string(),
+            api_key: "INSERT_API_KEY_HERE".to_string(),
+            quit_level: 130
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct Player {
+    name: String,
+    id: String
+}
+
+const CONFIG_PATH: &str = "config.toml";
+
+async fn read_config() -> Result<Config> {
+    let exists = match fs::try_exists("config.toml").await {
+        Ok(true) => true,
+        _ => false,
+    };
+
+    if !exists {
+        trace!("Creating config file at {CONFIG_PATH}");
+        let mut f = File::create(CONFIG_PATH).await?;
+
+        trace!("Generating default config");
+        let config = Config::default();
+        let config_str = toml::to_string(&config)?;
+        f.write_all(config_str.as_bytes()).await;
+    }
+
+    let config_str = std::fs::read_to_string(CONFIG_PATH)?;
+    let config: Config = toml::from_str(&config_str)?;
+    Ok(config)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    simple_logger::SimpleLogger::new().env().init().unwrap();
+
+    let config = Arc::new(read_config().await?);
+    let rt = Runtime::new()?;
+    let rt = Arc::new(rt);
+    
+
+    let mut hotwatch = Hotwatch::new()?;
+    hotwatch.watch(config.log_path.clone(), move |event| {
+        if let EventKind::Modify(_) = event.kind {
+            let log = std::fs::read_to_string(config.log_path.clone()).unwrap();
+            let lines: Vec<&str> = log.split("\n").collect();
+            let last_line = lines.last().unwrap();
+            let player_regex = Regex::new(r"\[.*:.*:.*\] \[.* thread\/INFO\]: \[CHAT\] ONLINE: ").unwrap();
+
+            if last_line.starts_with("[CHAT] ONLINE:") {
+                let cleaned_line = player_regex
+                    .replace_all(last_line, "")
+                    .replace('\r', "");
+
+                let rt = Arc::clone(&rt);
+                
+                let names: Vec<String> = cleaned_line.split(", ").map(|x| x.to_string()).collect();
+                // Only god knows why this works.
+                let value = config.clone();
+                rt.spawn(async move {
+                    let players = get_player_uuids(names).await.unwrap();
+
+                    for (_, uuid) in players {
+                        let config = value.clone();
+                        let hypixel_data = get_hypixel_data(uuid, config)
+                            .await
+                            .map_err(|e| {
+                                error!("Error while getting data from hypixel: {e}");
+                            })
+                            .unwrap();
+
+                        eprintln!("{:#?}", hypixel_data);
+                    }
+                });
+
+            }
+        }
+    })?;
+
+    Ok(())
+}
+
+async fn get_player_uuids(names: Vec<String>) -> Result<HashMap<String, Uuid>> {
+    let client = Client::new();
+    let chunks: Vec<&[String]> = names.chunks(10).collect();
+
+    let mut mojang_players: HashMap<String, Uuid> = HashMap::new();
+
+    for chunk in chunks {
+        let body = json!(chunk);
+        let response_res = client.post("https://api.minecraftservices.com/minecraft/profile/lookup/bulk/byname")
+            .header("content-type", "application/json")
+            .json(&body)
+            .send()
+            .await;
+        
+        if let Ok(resp) = response_res {
+            if !resp.status().is_success() {
+                handle_mojang_failure(&client, chunk, &mut mojang_players).await?;
+                continue;
+            }
+
+            let players: Vec<Player> = resp.json().await?;
+
+            for player in players {
+                mojang_players.insert(player.id, player.name);
+            }
+        }
+    };
+
+    Ok(mojang_players)
+}
+
+async fn handle_mojang_failure(client: &Client, chunk: &[String], mojang_players: &mut HashMap<String, Uuid>) -> Result<()> {
+    warn!("There was an error returned from Mojang API.");
+    warn!("Retrying using fallback api (api.minetools.eu)...");
+
+    for player in chunk {
+        let response_res = client.get(format!("https://api.minetools.eu/uuid/{}", player))
+            .send()
+            .await;
+
+        if let Ok(resp) = response_res {
+            let api_player: Player = resp.json().await?;
+            mojang_players.insert(api_player.id, api_player.name);
+        }
+    }
+
+    Ok(())
+}
+
+async fn get_hypixel_data(uuid: Uuid, config: Arc<Config>) -> Result<HypixelPlayer> {
+    let client = Client::new();
+    let response = client.get(format!("https://api.hypixel.net/v2/player?uuid={uuid}"))
+        .header("API-Key", &config.api_key)
+        .send()
+        .await;
+    
+    if let Ok(resp) = response {
+        let hypixel_data: ApiHypixelData = resp.json().await?;
+        if hypixel_data.player.is_some() {
+            return Ok(HypixelPlayer::from_api(hypixel_data.player.unwrap(), uuid));
+        }
+    }
+
+    anyhow::bail!("response is not ok");
+}
